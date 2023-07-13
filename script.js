f421a935d17c55c70fbc6eeccefddc530e47894e
Arnold W <arnoldwwong@gmail.com>
2023-07-14 01:14:51 +1000

    Accomodate for square borders

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
 const container = document.querySelector('#container');
 
-//function createSquare() {
-//    let square = document.createElement('div');
-//    square.classList.add('square');
-//    content.appendChild(square);
-//}
-
 function createGrid(x) {
     for (let i = 0; i < x ** 2; i++) {
-        //const content = document.createElement('div');
-        //content.classList.add('content');
-        //content.textContent = 'Let\'s make a grid!';
-        //content.textContent = 'Hi!';
-
-        //container.appendChild(content);
-        //for (j = 0; j < x; j++) {
-            const content = document.createElement('div');
-            content.classList.add('content');
-            content.style.width = (720 / x) + "px";
-            content.style.height = (720 / x) + "px";
-            //content.textContent = 'Bye!';
- 
-            container.appendChild(content);
-            //createSquare();
-        //}
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = ((720 / x) - 2) + "px";
+        square.style.height = ((720 / x) - 2) + "px";
+        container.appendChild(square);
     }
 }
 
