11b5d4914218063ef16e6da601bff03e4fc229e2
Arnold W <arnoldwwong@gmail.com>
2023-07-16 01:59:40 +1000

    Add clear function

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const container = document.querySelector('#container');
 
+//const button = document.querySelector('#reset');
+//    button.addEventListener('click', () => {
+//        resetGrid();
+//        let x = prompt();
+//        confirmLength();
+//        createGrid(x);
+//    });
+
+//function resetGrid() {
+//    document.getElementsByName('div').reset();
+//}
+
 function createGrid(x) {
     for (let i = 0; i < x ** 2; i++) {
-        const square = document.createElement('div');
+        let square = document.createElement('div');
         square.classList.add('square');
         square.style.width = ((720 / x) - 2) + "px";
         square.style.height = ((720 / x) - 2) + "px";
@@ -12,20 +24,32 @@ function createGrid(x) {
             e.target.style.background = 'steelblue';
         });
     }
+//    let clear = document.getElementById("clear");
+//    clear.onclick = function() {
+//    let squares = document.getElementsByClassName('square');
+//        for (let i = 0; i < x ** 2; i++) {
+//            squares[i].style.backgroundColor = 'white';
+//        }
+//    }
 }
 
-function getLength() {
+function clearGrid() {
+    let clear = document.getElementById("clear");
+    //clear.onclick = function() {
+    let squares = document.getElementsByClassName('square');
+        for (let i = 0; i < x ** 2; i++) {
+            squares[i].style.backgroundColor = 'white';
+        }
+    //}
+}
+
+function confirmLength() {
     while (x < 1 || x > 100 || isNaN(x)) {
         alert("Must be a number from 1 to 100.");
         x = prompt();
     }
-} 
+}
 
 let x = prompt();
-getLength();
+confirmLength();
 createGrid(x);
-
-//const div = document.querySelector('square');
-//div.addEventListener('mouseover', () => {
-//    div.style.backgroundColor = "blue";
-//});
